0d043005166afafc664a75af92c65e160be8691a
Cristian Moreno <cris.morenohurtado@gmail.com>
2024-11-28 16:32:35 -0500

    Added different injected methods.

---
+++ src/main/java/guru/springframework/sfg_di/controllers/ConstructorInjectedController.java
@@ -0,0 +1,15 @@
+package guru.springframework.sfg_di.controllers;
+
+import guru.springframework.sfg_di.services.GreetingService;
+
+public class ConstructorInjectedController {
+    private final GreetingService greetingService;
+
+    public ConstructorInjectedController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
+    public String getGreeting() {
+        return greetingService.sayGretting();
+    }
+}
---
+++ src/test/java/guru/springframework/sfg_di/controllers/SetterInjectedControllerTest.java
@@ -0,0 +1,21 @@
+package guru.springframework.sfg_di.controllers;
+
+import guru.springframework.sfg_di.services.GreetingServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class SetterInjectedControllerTest {
+
+    SetterInjectedController controller;
+
+    @BeforeEach
+    void setUp() {
+        controller = new SetterInjectedController();
+        controller.setGreetingService(new GreetingServiceImpl());
+    }
+
+    @Test
+    void getGreeting() {
+        System.out.println(controller.getGreeting());
+    }
+}
